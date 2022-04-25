4355afac94289fb3532c8208cecedb4a064b85cc
xcellnt <goatbooster@outlook.com>
2022-04-25 13:17:23 +0700

    first commit

---
+++ whatpluginsdo.sh
@@ -0,0 +1,39 @@
+FEATURES TO COVER:
+ - Fuzzy File Search
+ - Tag
+ - Autocomplete
+ - File Browsing
+ - Snippets
+ - Build Integration (if we have time)
+
+GOAT OF THIS TALK:
+ - Increase vim understanding
+ - Offer powerful options
+
+NOT GOALS OF THIS TALK:
+ - Hate on plugins
+ - Get people to stop using plugins
+
+BASIC SETUPS;
+
+ - Enter the current millenium
+  * set nocompatible = i dont really understand about it 
+ 
+ - enable syntax and plugins ( for netrw)
+  * syntax enable
+  * filetype plugin on
+
+FINDING FILES:
+
+ - Search down into subfolders
+ - Provides tab-completion for all file-related tasks
+  * set path+=**
+
+ - Display all matching files when we tab complete
+  * set wildmenu
+
+HOW WE CAN:
+ - Hit tab to :dind by partial match
+ - Use * to make it fuzzy
+
+8.46
