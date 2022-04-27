2cfe24c930d1aa78cf03d181ef34d8694cfe2454
xcellnt <goatbooster@outlook.com>
2022-04-27 08:52:21 +0700

    autocomplete

--- whatpluginsdo.sh
+++ whatpluginsdo.sh
@@ -26,7 +26,7 @@ BASIC SETUPS;
 FINDING FILES:
 
  - Search down into subfolders
- - Provides tab-completion for all file-related tasks
+  Provides tab-completion for all file-related tasks
   * set path+=**
 
  - Display all matching files when we tab complete
@@ -36,4 +36,31 @@ HOW WE CAN:
  - Hit tab to :dind by partial match
  - Use * to make it fuzzy
 
-8.46
+THINGS TO CONSIDER:
+ - :b lets u autocomplete any open buffer
+
+TAG JUMPING: 
+ - Create the 'tags' file (may need to install ctags first)
+  comand! MakeTags !ctags -R .
+
+HOW WE CAN:
+ - Use ^] to jump to tag under cursor
+ - Use g^] for ambigous tags
+ - Use ^t to jump back up the tag stack
+
+THINGS TO CONSIDER:
+ - This doesn't help if u want a visual list of tags
+
+AUTOCOMPLETE:
+ - The good stuff is documented in [ins-completion]
+
+HIGHLIHGTS:
+ - ^x^n for JUST this file
+ - ^x^f for filenames (works with our path trick!)
+ - ^x^j for tag only
+ - ^n for anything specified by the 'complete' option
+
+NOW WE CAN:
+ - Use ^n and ^p to go back and forth in the suggestion  list
+ 
+33.46
